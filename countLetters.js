7a725b53ae0fb88bf91c0b396535e5185b2c02bd
Clint Hiles <clinthiles1992@gmail.com>
2024-06-26 15:43:26 -0700

    Kinda getting it

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+//The console.assert() static method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
+/*const sum = function (a, b) {
+  return a + b;
+};
+
+console.assert(sum(1, 2 ) === 3);
+console.assert(sum(1, 20) === 3);
+
+const sumBuggy = function(a, b) {
+  return a * b;
+};
+
+console.assert(sumBuggy(1, 2) === 3);
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function(letters) {
+  let count = {};
+  for (let letter of letters) {
+    if (count[letter]) {
+      count[letter] ++;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  return count;
+};
+
+console.log(countLetters("LHL"));
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
