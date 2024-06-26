8338f00c532e354735cd25b924e296e53c82ab08
Clint Hiles <clinthiles1992@gmail.com>
2024-06-25 17:59:38 -0700

    writing code to see if things match

---
+++ eqArrays.js
@@ -0,0 +1,44 @@
+//The console.assert() static method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
+/*const sum = function (a, b) {
+  return a + b;
+};
+
+console.assert(sum(1, 2 ) === 3);
+console.assert(sum(1, 20) === 3);
+
+const sumBuggy = function(a, b) {
+  return a * b;
+};
+
+console.assert(sumBuggy(1, 2) === 3);
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// eqArrays.js
+
+const eqArrays  = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); //
