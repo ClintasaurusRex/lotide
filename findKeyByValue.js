da6c35a81bedc87a7cc320372ce24657a09714df
Clint Hiles <clinthiles1992@gmail.com>
2024-06-26 17:48:51 -0700

    All on my own

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  const keys = Object.keys(obj);
+  for (let key of keys) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70' Show"), undefined);
