5f7b9d011b7645054115756ffd041ee6127d2077
Clint Hiles <clinthiles1992@gmail.com>
2024-07-01 16:19:56 -0700

    re refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,13 @@
 const middle = require("../middle");
-const assertArrayEqual = require("../assertArraysEqual");
-
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4,]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+const assertArraysEqual = require("../assertArraysEqual");
 
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]); // should PASS
+// console.log(middle([1]));
+// console.log(middle([1, 2]));
+// console.log(middle([1, 2, 3]));
+// console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4,]));
+// console.log(middle([1, 2, 3, 4, 5, 6]));
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should PASS
+assertArraysEqual(middle([1, 2, 3]), [2]);
