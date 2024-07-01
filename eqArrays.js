a9dca3abe1e87f88057418cdeae80b8db077947d
Clint Hiles <clinthiles1992@gmail.com>
2024-07-01 14:35:51 -0700

    Exporting file to test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,3 @@
-//The console.assert() static method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
-/*const sum = function (a, b) {
-  return a + b;
-};
-
-console.assert(sum(1, 2 ) === 3);
-console.assert(sum(1, 20) === 3);
-
-const sumBuggy = function(a, b) {
-  return a * b;
-};
-
-console.assert(sumBuggy(1, 2) === 3);
-*/
-
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 // eqArrays.js
 
@@ -39,6 +14,9 @@ const eqArrays  = function(arr1, arr2) {
 };
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); //
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should PASS
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); //
+
+
+module.exports = eqArrays;
