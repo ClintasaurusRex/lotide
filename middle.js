aa3740de2b55678cd61c4aaf558fe8dc89a0ce60
Clint Hiles <clinthiles1992@gmail.com>
2024-07-01 16:05:33 -0700

    Refactored

--- middle.js
+++ middle.js
@@ -1,28 +1,6 @@
+const assertArraysEqual = require("./assertArraysEqual");
 
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArrayEqual = (actual, expected) => {
-  if (eqArrays(actual, expected)) {
-    //Arrays are equal
-    console.log(`✅✅✅ Assertion Passed: 
-      ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-  } else {
-    //Arrays are not equal
-    console.log(`🛑🛑🛑 Assertion Failed: 
-      ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
-  }
-};
 
 const middle = function(array) {
   if (array.length <= 2) {
@@ -37,15 +15,16 @@ const middle = function(array) {
   return [array[mid]];// odd nums
 };
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]); // should PASS
-assertArrayEqual([1, 2, 3], [3, 2, 1]); // should PASS
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArraysEqual(middle([1, 2, 3], [1, 2, 3])); // should PASS
+// assertArraysEqual(middle([1, 2, 3], [3, 2, 1])); // should PASS
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 
 
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4,]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+// console.log(middle([1]));
+// console.log(middle([1, 2]));
+// console.log(middle([1, 2, 3]));
+// console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4,]));
+// console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle;
