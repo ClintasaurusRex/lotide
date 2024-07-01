7af352e5134778b92fb125cd73f629e6cfb6709a
Clint Hiles <clinthiles1992@gmail.com>
2024-07-01 12:51:14 -0700

    added another test

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,5 +3,7 @@ const tail = require("../tail");
 const assertEqual = require("../assertEqual");
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
+const words1 = ['welcome', 'to', 'the', 'jungle'];
+tail(words, words1);
 assertEqual(words.length, 3);
+assertEqual(words1.length, 3);
