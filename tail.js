eaafe4fa16326fe4eb24f460c77047d96b260f2a
Clint Hiles <clinthiles1992@gmail.com>
2024-07-01 12:45:19 -0700

    Updated tail

--- tail.js
+++ tail.js
@@ -1,18 +1,15 @@
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-};
 
-const tail = function (arr) {
-  if(!Array.isArray(arr))  {
+
+const tail = function(arr) {
+  if (!Array.isArray(arr))  {
     return undefined;
   }
   return arr.length > 1 ? arr.slice(1) : [];
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+
+module.exports = tail;
