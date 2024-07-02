a9ffda983b9eb0404680541427113816e159bbf6
Clint Hiles <clinthiles1992@gmail.com>
2024-07-01 18:12:50 -0700

    Running tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,37 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+
+const assert = require("chai").assert;
+
+
+describe("#middle", () => {
+  it("returns the middle number of an odd-length array", () => {
+    assert.deepEqual(middle(["1", "2", "3"]), ["2"]);
+  });
+
+  it("returns the middle two numbers for an odd-length array", () => {
+    assert.deepEqual(middle(['1', '2', '3', '4', '5']), ['3']);
+  });
+
+  it("returns the middle numbers of an even-length array", () => {
+    assert.deepEqual(middle(['1', '2', '3', '4']), ['2', '3']);
+  });
+});
+
+
+
+
+
+
+
+
+
+
 
 
 // console.log(middle([1]));
 // console.log(middle([1, 2]));
-// console.log(middle([1, 2, 3]));
-// console.log(middle([1, 2, 3, 4, 5]));
-// console.log(middle([1, 2, 3, 4,]));
+// console.log(middle([1, 2, 3])); [2]
+// console.log(middle([1, 2, 3, 4, 5]));[3]
+// console.log(middle([1, 2, 3, 4,]));[2, 3]
 // console.log(middle([1, 2, 3, 4, 5, 6]));
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // should PASS
-assertArraysEqual(middle([1, 2, 3]), [2]);
